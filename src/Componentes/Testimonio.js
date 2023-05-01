c9fdfe22ef190cfe06f7fc1dcb71e1a8ee9a36a8
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-05-01 19:36:14 -0300

    primer commit

---
+++ src/Componentes/Testimonio.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(){
+  return(
+    <div className='contenedor-testimonio'>
+      <img 
+      className='imagen-testimonio'
+      src={require('../Imagenes/testimonio-emma.png')}
+      alt='Fotografia de Emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>Emma Bostian in Sweden</p>
+        <p className='cargo-testimonio'>Software Engineer at Spotify</p>
+        <p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+
+      </div>
+
+    </div> 
+
+  );
+
+}
+
+export default Testimonio;
