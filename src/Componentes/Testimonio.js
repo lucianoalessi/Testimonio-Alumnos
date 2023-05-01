b016d871485e70ae6ecb15b794381e9faf7955ef
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-05-01 19:56:56 -0300

    listo

--- src/Componentes/Testimonio.js
+++ src/Componentes/Testimonio.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
   return(
     <div className='contenedor-testimonio'>
       <img 
       className='imagen-testimonio'
-      src={require('../Imagenes/testimonio-emma.png')}
+      src={require(`../Imagenes/testimonio-${props.imagen}.png`)}
       alt='Fotografia de Emma'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian in Sweden</p>
-        <p className='cargo-testimonio'>Software Engineer at Spotify</p>
-        <p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> in <strong>{props.pais}</strong></p>
+        <p className='cargo-testimonio'><strong>{props.cargo}</strong> at <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
 
       </div>
 
